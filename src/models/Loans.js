0096a3d7b66ab8ebdea7ad5eeb74d26ae611518d
Felipe <93364922+FelipeGranobles@users.noreply.github.com>
2025-05-25 22:26:53 -0500

    Cambios en el modelo de prestamos

--- src/models/Loans.js
+++ src/models/Loans.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const LoanDetailSchema = new Schema({
-  libro: { type: Schema.Types.ObjectId, ref: "Books", required: true },
-  titulo_libro: { type: String, required: true }, 
+  book: { type: Schema.Types.ObjectId, ref: "Books", required: true },
+  book_title: { type: String, required: true },
 });
 
 const LoanSchema = new Schema({
-  fecha_prestamo: { type: Date, default: Date.now },
-  fecha_devolucion: { type: Date, required: true },
-  usuario: { type:String, required: true },
-  estado_prestamo: { type: String, enum: ["activo", "finalizado", "vencido"], default: "activo" },
-  detalles: [LoanDetailSchema] 
+  User: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+  Loan_date: { type: Date, default: Date.now },
+  Devolution_date: { type: Date, required: true },
+  Loan_state: { type: String, required: true },
+  Details: [LoanDetailSchema],
 });
 
 module.exports = mongoose.model("Loans", LoanSchema);
