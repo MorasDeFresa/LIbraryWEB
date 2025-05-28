76ba038e017c364c6006d520a1ef73a8f7e2b2df
MorasDeFresa <bencito10@gmail.com>
2025-05-28 18:51:11 -0500

    🐞 fix: delete unnecesary console log

--- src/index.js
+++ src/index.js
@@ -70,8 +70,3 @@ app.use(require("./routes/loans"));
 
 //Static Files
 app.use(express.static(path.join(__dirname, "public")));
-
-//Server is listening
-app.listen(app.get("port"), () => {
-  console.log("El server esta en el puerto: ", app.get("port"));
-});
